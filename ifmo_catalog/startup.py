9a628a7aafe7a505e477748f884753b25029d451
KuznetcovaNa <luckyginger95@mail.ru>
2014-11-27 21:26:07 +0300

    modified:   ../startup.py 	modified:   index.html

---
+++ ifmo_catalog/startup.py
@@ -0,0 +1,32 @@
+from django.conf import settings
+from django.conf.urls import include, url
+from path import path
+
+
+import edxmako
+
+
+def patch_templates():
+    # TODO Add template path to template look up path collection in settings
+    template_path = path(__file__).dirname() / 'templates'
+    edxmako.paths.add_lookup('main', template_path, prepend=True)
+    if settings.SERVICE_VARIANT == 'cms':
+        edxmako.paths.add_lookup('lms.main', template_path, prepend=True)
+
+
+def _patch_variant_url(app_name, urls_module):
+    app_urls = __import__('%s.urls' % (app_name,), fromlist=['urlpatterns'])
+    app_urls.urlpatterns.insert(0, url(r'', include(urls_module)))
+
+
+def patch_urls():
+    try:
+        _patch_variant_url('lms', 'ifmo_catalod.urls')
+        _patch_variant_url('cms', 'ifmo_catalog.urls')
+    except Exception:
+        pass
+
+
+def run():
+    patch_templates()
+    patch_urls()

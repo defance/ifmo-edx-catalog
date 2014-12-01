8f685866dc1cb6e6d364818628c1158026f4ed02
KuznetcovaNa <luckyginger95@mail.ru>
2014-12-01 04:38:40 +0300

    new file:   static/css/bootstrap-theme.css 	new file:   static/css/bootstrap.css 	new file:   static/fonts/glyphicons-halflings-regular.eot 	new file:   static/fonts/glyphicons-halflings-regular.svg 	new file:   static/fonts/glyphicons-halflings-regular.ttf 	new file:   static/fonts/glyphicons-halflings-regular.woff 	new file:   static/js/bootstrap.js 	new file:   static/js/jquery.min.js 	modified:   templates/edit_catalog.html 	modified:   urls.py 	modified:   views.py

--- ifmo_catalog/urls.py
+++ ifmo_catalog/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns, include, url
 
 
 urlpatterns = patterns('',
-    url(r'^course/edit_catalog$', 'ifmo_catalog.views.edit_catalog'),
+    url(r'^course/edit_catalog', 'ifmo_catalog.views.edit_catalog'),
     url(r'^dashboard/try1', 'ifmo_catalog.views.try1'),
 )

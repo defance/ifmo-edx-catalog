cb2b5317b0c057907fddb22bb347ff90813dcf32
KuznetcovaNa <luckyginger95@mail.ru>
2014-12-08 03:06:27 +0300

    modified:   views.py

--- ifmo_catalog/views.py
+++ ifmo_catalog/views.py
@@ -16,12 +16,13 @@ def all_courses(request):
 def edit_catalog_handler(request):
     label_list = LabelForCourses.objects.all()
     command = request.POST.get('command')
-    #if command == 'delete_label':
-        #label_id = int(request.POST.get('label_id'))
-        #for label in label_list:
-            #if label.parent.id == label_id:
-                #del_obj = label_list.get(pk=label.id)
-                #del_obj.delete()
-        #del_obj_root = label_list.get(pk=label_id)
-        #del_obj_root.delete()
-    return render_to_response('edit_catalog.html', {'label_list': label_list})
+    if command == 'delete_label':
+        label_id = int(request.POST.get('label_id'))
+        label1 = label_list[0]
+        for label in label_list:
+            if not label1.id == label.id:
+                if label.parent.id == label_id:
+                    label.delete()
+        del_obj_root = label_list.get(pk=label_id)
+        del_obj_root.delete()
+        return HttpResponse(command)

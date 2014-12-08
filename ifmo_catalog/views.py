dac1f0d0265b1c40f06be87ee4ca1115ee976a4e
KuznetcovaNa <luckyginger95@mail.ru>
2014-12-08 06:44:10 +0300

    modified:   templates/edit_catalog.html 	modified:   views.py

--- ifmo_catalog/views.py
+++ ifmo_catalog/views.py
@@ -15,10 +15,10 @@ def all_courses(request):
 
 def edit_catalog_handler(request):
     label_list = LabelForCourses.objects.all()
+    label1 = label_list[0]
     command = request.POST.get('command')
     if command == 'delete_label':
         label_id = int(request.POST.get('label_id'))
-        label1 = label_list[0]
         for label in label_list:
             if not label1.id == label.id:
                 if label.parent.id == label_id:
@@ -26,3 +26,16 @@ def edit_catalog_handler(request):
         del_obj_root = label_list.get(pk=label_id)
         del_obj_root.delete()
         return HttpResponse(command)
+    elif command == 'edit_label':
+        label_id = int(request.POST.get('label_id'))
+        edit_obj = label_list.get(pk=label_id)
+        name_input = request.POST.get('name_input')
+        if name_input == '':
+            msg = 'Empty new name'
+        for label in label_list:
+            if not label1.id == label.id:
+                if name_input == label.name_of_label and label.parent.id == edit_obj.parent.id:
+                    msg = 'This name is already in this branch'
+        edit_obj.name_of_label = name_input
+        edit_obj.save()
+        return HttpResponse(msg)
